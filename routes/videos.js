9e765a8446e0ceb46e42655b9cea9185ba51cddb
Hany Ayad <hanyayad71@gmail.com>
2023-02-16 12:15:27 +0100

    ready to deploy api

---
+++ routes/videos.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  addVideo,
+  addView,
+  getByTag,
+  getVideo,
+  random,
+  search,
+  sub,
+  trend,
+  getById,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, addVideo);
+router.delete("/:id", verifyToken, addVideo);
+router.get("/find/:id", getVideo);
+router.put("/view/:id", addView);
+router.get("/trend", trend);
+router.get("/random", random);
+router.get("/sub", verifyToken, sub);
+router.get("/tags", getByTag);
+router.get("/search", search);
+router.post("/getById", getById);
+export default router;
